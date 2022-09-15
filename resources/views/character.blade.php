634822c9251f8065605323aa1d6a9b375b862f87
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-15 18:04:42 +0200

    header e footer fatti

--- resources/views/character.blade.php
+++ resources/views/character.blade.php
@@ -6,10 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    {{-- css --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
     @include('includes.header')
+    <main>
+        <h1>
+            ciaoooo mondo
+        </h1>
+    </main>
+    @include('includes.footer')
 </body>
 
 </html>
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,78 @@
+ <footer>
+     <div class="outer-footer">
+         <div class="container half">
+             <div class="flex-row">
+                 <div class="flex-c-col">
+                     <h3>DC Comics</h3>
+                     <ul>
+                         <li><a href="#">characters</a></li>
+                         <li><a href="#">comics</a></li>
+                         <li><a href="#">movies</a></li>
+                         <li><a href="#">Tv</a></li>
+                         <li><a href="#">games</a></li>
+                         <li><a href="#">videos</a></li>
+                         <li><a href="#">news</a></li>
+                     </ul>
+                     <h3>Shop</h3>
+                     <ul>
+                         <li><a href="#">Shop DC</a></li>
+                         <li><a href="#">Shop DC</a></li>
+                         <li><a href="#">Collectibles</a></li>
+                     </ul>
+                 </div>
+                 <div class="flex-c-col">
+                     <h3>DC</h3>
+                     <ul>
+                         <li><a href="#">Term of use</a></li>
+                         <li><a href="#">privacy policy</a></li>
+                         <li><a href="#">ad choice</a></li>
+                         <li><a href="#">advertising</a></li>
+                         <li><a href="#">jobs</a></li>
+                         <li><a href="#">subscription</a></li>
+                         <li><a href="#">talent workshops</a></li>
+                         <li><a href="#">cpsc certificate</a></li>
+                         <li><a href="#">rating</a></li>
+                         <li><a href="#">shop help</a></li>
+                         <li><a href="#">contact us</a></li>
+                     </ul>
+                 </div>
+                 <div class="flex-c-col">
+                     <h3>Sites</h3>
+                     <ul>
+                         <li><a href="#">DC</a></li>
+                         <li><a href="#">mad magazine</a></li>
+                         <li><a href="#">dc kids</a></li>
+                         <li><a href="#">dc universe</a></li>
+                         <li><a href="#">dc power visa</a></li>
+                     </ul>
+                 </div>
+             </div>
+         </div>
+         <div class="logo-bg"></div>
+     </div>
+     <div class="inner-footer">
+         <div class="container">
+             <div class="flex-row justify-between">
+                 <a class="subscribe">SIGN-UP NOW!</a>
+                 <div class="social flex-row">
+                     <a href="#" class="pad-lr">FOLLOW US</a>
+                     <figure class="pad-lr">
+                         <img src="{{ asset('images/footer-facebook.png') }}" alt="social">
+                     </figure>
+                     <figure class="pad-lr">
+                         <img src="{{ asset('images/footer-periscope.png') }}" alt="social">
+                     </figure>
+                     <figure class="pad-lr">
+                         <img src="{{ asset('images/footer-pinterest.png') }}" alt="social">
+                     </figure>
+                     <figure class="pad-lr">
+                         <img src="{{ asset('images/footer-twitter.png') }}" alt="social">
+                     </figure>
+                     <figure class="pad-lr">
+                         <img src="{{ asset('images/footer-youtube.png') }}" alt="social">
+                     </figure>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </footer>
