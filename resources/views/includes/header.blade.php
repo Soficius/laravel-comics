58d1e11d4c4ac9971a1da15477e4f5d3046cf2fd
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-15 16:56:10 +0200

    aggiunti i componenti blade

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,19 @@
+ <header class="container">
+     <figure>
+         <a href="#"><img src="{{ asset('images/dc-logo.png') }}" alt="logo"></a>
+     </figure>
+     <nav class="navbar">
+         <ul>
+             <li> <a href="{{ route('character') }}">character</a></li>
+             <li> <a href="{{ route('comics') }}">comics</a></li>
+             <li> <a href="{{ route('movies') }}">movies</a></li>
+             <li> <a href="{{ route('tv') }}">tv</a></li>
+             <li> <a href="{{ route('games') }}">games</a></li>
+             <li> <a href="{{ route('collectibles') }}">collectibles</a></li>
+             <li> <a href="{{ route('videos') }}">videos</a></li>
+             <li> <a href="{{ route('fans') }}">fans</a></li>
+             <li> <a href="{{ route('news') }}">news</a></li>
+             <li> <a href="{{ route('shop') }}">shop</a></li>
+         </ul>
+     </nav>
+ </header>
