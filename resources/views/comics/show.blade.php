6d933577e65978847f8c3e59b40b26448de01979
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-18 19:43:05 +0200

    aggiunto css

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,10 +1,35 @@
 @extends('layout.main')
 @section('main-content')
-    <div class="container bg-white">
+    <div class="small-container bg-white">
         <h1>{{ $comic['title'] }}</h1>
-        <div class="details w-50 d-flex">
-            <p class="p-4">{{ $comic['description'] }}</p>
+        <div class="details d-flex">
+            <p class="w-75">{{ $comic['description'] }}</p>
             <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+        </div>
+        <div class="wrap">
+            <div class="w-50 p-r">
+                <h3>Talent</h3>
+                <div>
+                    Art by:
+                    @foreach ($comic['artists'] as $artist)
+                        {{ $artist }},
+                    @endforeach
+                </div>
+                <div>
+                    Written By:
+                    @foreach ($comic['writers'] as $writer)
+                        {{ $writer }},
+                    @endforeach
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h3>Specs</h3>
+                    <p>Series: {{ $comic['type'] }}</p>
+                    <p>U.s Price: {{ $comic['price'] }}</p>
+                    <p>On Sale Date: {{ $comic['sale_date'] }}</p>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
