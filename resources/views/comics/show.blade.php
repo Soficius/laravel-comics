d7cf9c4ebc87543739627f222de4adb3e256f8ac
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-19 10:14:55 +0200

    task done

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,30 +6,75 @@
             <p class="w-75">{{ $comic['description'] }}</p>
             <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
         </div>
+    </div>
+    <div class="small-container bg-gray">
         <div class="wrap">
-            <div class="w-50 p-r">
-                <h3>Talent</h3>
-                <div>
-                    Art by:
-                    @foreach ($comic['artists'] as $artist)
-                        {{ $artist }},
-                    @endforeach
+            <div class=" p-r flex-c-col w-47">
+                <h3 class="ptb-1">Talent</h3>
+                <hr />
+                <div class="flex-c-row ptb-1">
+                    <div class="w-30">
+                        Art by:
+                    </div>
+                    <div class="w-70">
+                        @foreach ($comic['artists'] as $artist)
+                            {{ $artist }},
+                        @endforeach
+                    </div>
                 </div>
-                <div>
-                    Written By:
-                    @foreach ($comic['writers'] as $writer)
-                        {{ $writer }},
-                    @endforeach
+                <hr />
+                <div class="flex-c-row ptb-1">
+                    <div class="w-30">
+                        Written By:
+                    </div>
+                    <div class="w-70">
+                        @foreach ($comic['writers'] as $writer)
+                            {{ $writer }},
+                        @endforeach
+                    </div>
                 </div>
+                <hr />
             </div>
-            <div>
-                <div>
-                    <h3>Specs</h3>
-                    <p>Series: {{ $comic['type'] }}</p>
-                    <p>U.s Price: {{ $comic['price'] }}</p>
-                    <p>On Sale Date: {{ $comic['sale_date'] }}</p>
+            <div class="flex-c-col w-47">
+                <h3 class="ptb-1">Specs</h3>
+                <hr />
+                <div class="flex-c-row ptb-1">
+                    <div class="w-30">Series:</div>
+                    <div class="w-70">{{ $comic['type'] }}</div>
                 </div>
+                <hr>
+                <div class="flex-c-row ptb-1">
+                    <div class="w-30">U.s Price:</div>
+                    <div class="w-70">
+                        {{ $comic['price'] }}
+                    </div>
+                </div>
+                <hr>
+                <div class="flex-c-row ptb-1">
+                    <div class="w-30">On Sale Date:</div>
+                    <div class="w-70">{{ $comic['sale_date'] }}</div>
+                </div>
+                <hr>
             </div>
+        </div>
+    </div>
+    <div class="small-c-plr flex-c-row bg-gray border-top">
+        <div class="flex-c-row w-25 j-between border-lr p-1">
+            <span>DIGITAL COMICS</span>
+            <i class="cta-pad-normal"></i>
+        </div>
+        <div class="flex-c-row w-25 j-between border-r p-1">
+            <span>SHOP DC</span>
+            <i class="cta-comics-normal"></i>
+        </div>
+        <div class="flex-c-row w-25 j-between border-r p-1">
+            <span>COMIC SHOP LOCATOR</span>
+            <i class="cta-pointer-normal"></i>
+        </div>
+        <div class="flex-c-row w-25 j-between border-r p-1">
+            <span>SUBSCRIPTIONS</span>
+            <i class="cta-shirt-normal"></i>
+
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('home');
+});
+
 // character
 Route::get('/character', function () {
     return view('character');
